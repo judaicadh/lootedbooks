9e74d118bb0b495a45f7f2eb4271683a0029e480
judaicadhpenn <157063908+judaicadhpenn@users.noreply.github.com>
2026-07-13 15:11:17 -0700

    added

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import netlify from '@astrojs/netlify';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  adapter: netlify()
+});
